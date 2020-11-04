463d6a3ea4d6e003ec6df6d122a90c3a3f3da6d1
Eduardo Guzmán Hermandez <eduarguzher@gmail.com>
2020-11-04 15:21:59 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -60,4 +60,21 @@ module.exports = {
         backgroundColor: ({ after }) => after(['active']),
         textColor: ({ after }) => after(['active']),
     },
+    purge: {
+        content: [
+            './app/**/*.php',
+            './resources/**/*.html',
+            './resources/**/*.js',
+            './resources/**/*.jsx',
+            './resources/**/*.ts',
+            './resources/**/*.tsx',
+            './resources/**/*.php',
+            './resources/**/*.vue',
+            './resources/**/*.twig',
+        ],
+        options: {
+            defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+            whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+        },
+    },
 };
